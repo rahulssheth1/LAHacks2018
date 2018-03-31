17be9b252c48267f84e8a9e0cd4b4ac7c52f700e
Rahul Sheth <Rahul@Rahuls-MacBook-Pro.local>
2018-03-31 00:34:56 -0700

    Starter Front End Code

--- LAHacks2018/ViewController.swift
+++ LAHacks2018/ViewController.swift
@@ -10,8 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @objc func segueToStudent() {
+        let studentViewController = StudentViewController()
+        self.present(studentViewController, animated: true, completion: nil)
+    }
+    @objc func segueToProf() {
+        let profViewController = ProfessorViewController()
+        self.present(profViewController, animated: true, completion: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        // Student versus professor choosing
+        
+        
+        let studentButton = UIButton()
+        self.view.addSubview(studentButton)
+        studentButton.translatesAutoresizingMaskIntoConstraints = false
+        studentButton.setTitle("Student", for: .normal)
+        studentButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        studentButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: -30).isActive = true
+        studentButton.widthAnchor.constraint(equalToConstant: self.view.bounds.width * 0.5).isActive = true
+        studentButton.heightAnchor.constraint(equalToConstant: self.view.bounds.width * 0.1).isActive = true
+        studentButton.addTarget(self, action: #selector(segueToStudent), for: .touchUpInside)
+        studentButton.backgroundColor = UIColor.blue
+        let professorButton = UIButton()
+        self.view.addSubview(professorButton)
+        
+        professorButton.translatesAutoresizingMaskIntoConstraints = false
+        professorButton.titleLabel?.text = "Professor"
+        professorButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        professorButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+        professorButton.widthAnchor.constraint(equalToConstant: self.view.bounds.width * 0.5).isActive = true
+        professorButton.setTitle("Professor", for: .normal)
+        professorButton.heightAnchor.constraint(equalToConstant: self.view.bounds.width * 0.1).isActive = true
+        professorButton.addTarget(self, action: #selector(segueToProf), for: .touchUpInside)
+        professorButton.backgroundColor = UIColor.blue
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'LAHacks2018' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for LAHacks2018
+
+  target 'LAHacks2018Tests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'LAHacks2018UITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
