e2f4bb2bf17290188eea3bfee7178b990c7d12a4
Rahul Sheth <Rahul@rahuls-macbook-pro.local>
2018-03-30 23:58:56 -0700

    Initial Commit

---
+++ LAHacks2018/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LAHacks2018
+//
+//  Created by Rahul Sheth on 3/30/18.
+//  Copyright © 2018 Rahul Sheth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
